6a1e34b704f369687b9fc260de821c7f7389911b
Robin Pokorný <robin@medvedi.eu>
2014-06-01 12:49:49 +0200

    Add new favicon

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,13 +1,14 @@
 /*jslint evil: true */
+/*exported mail2 */
 function mail2(name, dom, tl, params, display) {
     'use strict';
 
     // Email.js version 5
     var tld = [],
-        m = "mailto:",
-        a = "@",
-        d = ".";
-    tld[0] = "com";
+        m = 'mailto:',
+        a = '@',
+        d = '.';
+    tld[0] = 'com';
 
     document.write('<a href="' + m + name + a + dom + d + tld[tl] + params + '">' + display + '</a>');
 }
