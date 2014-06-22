b4430c25a4b15b31f98a3fc87ab303c175700b4b
Robin Pokorný <robin@medvedi.eu>
2014-06-22 14:59:17 +0200

    Rewrite mail2 JS function

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,14 +1,19 @@
 /*jslint evil: true */
 /*exported mail2 */
-function mail2(name, dom, tl, params, display) {
-    'use strict';
+function mail2(name, dom, tl, params, display, where) {
+  'use strict';
 
-    // Email.js version 5
-    var tld = [],
-        m = 'mailto:',
-        a = '@',
-        d = '.';
-    tld[0] = 'com';
+  // Email.js version 5
+  var tld = [],
+    m = 'mailto:',
+    a = '@',
+    d = '.',
+    e = document.createElement('a');
 
-    document.write('<a href="' + m + name + a + dom + d + tld[tl] + params + '">' + display + '</a>');
+  tld[0] = 'com';
+
+  e.href =  m + name + a + dom + d + tld[tl] + params;
+  e.innerHTML = display;
+
+  document.getElementById(where).appendChild(e);
 }
