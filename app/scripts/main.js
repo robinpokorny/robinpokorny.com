526334ed56b07c65a6de294405f3e5ca55c38b6e
Robin Pokorny <Robin.Pokorny@lmc.eu>
2014-10-27 12:00:44 +0100

    Change greeting

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,17 +1,17 @@
 /*jslint evil: true */
 /*exported mail2 */
 function mail2(name, dom, tl, params, display, where) {
-  'use strict';
+    'use strict';
 
-  // Email.js version 5
-  var tld = ['com'],
+    // Email.js version 5
+    var tld = ['com'],
     m = 'mailto:',
     a = '@',
     d = '.',
     e = document.createElement('a');
 
-  e.href =  m + name + a + dom + d + tld[tl] + params;
-  e.innerHTML = display;
+    e.href =  m + name + a + dom + d + tld[tl] + params;
+    e.innerHTML = display;
 
-  document.getElementById(where).appendChild(e);
+    document.getElementById(where).appendChild(e);
 }
